a79821f605f45993bff85ac9832c6b4d05f06fe3
yye <yukuiye@scannizaromacbo.mshanken.com>
2014-11-04 11:47:11 -0500

    Initial Commit

---
+++ Candy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Candy
+//
+//  Created by yye on 11/4/14.
+//  Copyright (c) 2014 MindCandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Candy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Candy
+//
+//  Created by yye on 11/4/14.
+//  Copyright (c) 2014 MindCandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
